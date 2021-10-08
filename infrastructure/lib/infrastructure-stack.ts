3c1309d244537e7bd06b199b1eb072d79713ee79
unknown <broughton.24@gmail.com>
2021-10-08 09:15:48 -0400

    Added deployment infrastructure

---
+++ infrastructure/lib/infrastructure-stack.ts
@@ -0,0 +1,15 @@
+import * as cdk from '@aws-cdk/core';
+import { SPADeploy } from 'cdk-spa-deploy';
+
+export class InfrastructureStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new SPADeploy(this, 'websiteDeploy')
+      .createSiteWithCloudfront({
+        indexDoc: 'index.html',
+        websiteFolder: '../webapp/out'
+      })
+    }
+
+}
